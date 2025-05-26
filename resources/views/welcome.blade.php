4f5203f9b31d5a8a8b66fd618639bf19f28c65be
Alnuzaili <abdulrahman.nuzaili@gmail.com>
2025-05-26 09:53:44 +0300

    feat: add filament

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -269,6 +269,9 @@
                 </div>
             </main>
         </div>
+            <a href="{{ url('/admin') }}" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
+                Admin Panel
+            </a>
 
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
