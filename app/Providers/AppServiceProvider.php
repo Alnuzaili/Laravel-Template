518f6e0305dd7386debb867b8eb9e2ac16dc2a99
Alnuzaili <abdulrahman.nuzaili@gmail.com>
2025-06-12 23:42:14 +0300

    feat: add Language Switch in admin panel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,15 @@
 
 namespace App\Providers;
 
+use App\Events\LoggedIn;
+use App\Listeners\UserLocalChangeListener;
+use App\Listeners\UserLoggedInListener;
+use BezhanSalleh\FilamentLanguageSwitch\Enums\Placement;
+use BezhanSalleh\FilamentLanguageSwitch\Events\LocaleChanged;
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
+use Filament\Facades\Filament;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +28,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole('super_admin') ? true : null;
+        });
+
+        Event::listen(
+            LocaleChanged::class,
+            [UserLocalChangeListener::class, 'handle']
+        );
+
+        Event::listen(
+            LoggedIn::class,
+            [UserLoggedInListener::class, 'handle']
+        );
+        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+            $switch
+                ->visible(outsidePanels: true)
+                ->userPreferredLocale(fn() => Filament::auth()->user()->locale ?? 'ar')
+                // ->renderHook(PanelsRenderHook::USER_MENU_AFTER)
+                ->outsidePanelPlacement(Placement::TopLeft)
+                ->locales(['ar', 'en']); // also accepts a closure
+        });
     }
 }
